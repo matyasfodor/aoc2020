c8cc4a4e5a5306508e43083456141178574e090b
Matyas Fodor <matyas.fodor@benevolent.ai>
2020-12-01 07:53:07 +0100

    feat: d01 second start

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -16,6 +17,7 @@ where
 fn main() {
     let matches = App::new("AOC solution 1")
         .arg(Arg::with_name("test").short("t").long("test"))
+        .arg(Arg::with_name("second").short("s").long("second"))
         .get_matches();
 
     let path = if matches.is_present("test") {
@@ -23,6 +25,7 @@ fn main() {
     } else {
         "input.txt"
     };
+    let num_previous_elements = if matches.is_present("second") { 2 } else { 1 };
     let target = 2020;
     let mut vec: Vec<i32> = Vec::new();
     if let Ok(lines) = read_lines(path) {
@@ -32,14 +35,17 @@ fn main() {
                     .trim()
                     .parse::<i32>()
                     .expect("expected a number");
-                // println!("With text:\n{} {}", price, cnt);
-                for prev_price in vec.iter() {
-                    if price + prev_price == target {
+                // If the 's' flag is not passed, it uses combinations of 1
+                // (which is the same as a plain iterator)
+                // If the 's'  flag is passed, it takes combinations of 2 from the vector
+                for prev_price in vec.iter().combinations(num_previous_elements) {
+                    // combinations return an array of reference objects, they need to be de-referenced before summed up
+                    if prev_price.iter().map(|x| (*x)).sum::<i32>() == target - price {
                         println!(
-                            "Found: {} {}, their product is {}",
+                            "Found: {} {:?}, their product is {}",
                             price,
                             prev_price,
-                            price * prev_price
+                            price * prev_price.iter().map(|x| (*x)).product::<i32>()
                         );
                     }
                 }
