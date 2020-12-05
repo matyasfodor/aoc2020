91a1a2acf0b387dd3d239fe2e5caf5633988b8b8
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-05 15:49:57 +0100

    feat: d4 passing

--- d04/src/main.rs
+++ d04/src/main.rs
@@ -1,7 +1,9 @@
 use clap::{App, Arg};
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::iter::FromIterator;
 use std::path::Path;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -27,24 +29,39 @@ impl<'a> PassportGenerator<'a> {
 }
 
 impl<'a> Iterator for PassportGenerator<'a> {
-    type Item = &'a str;
+    type Item = bool;
     fn next(&mut self) -> Option<Self::Item> {
-        let mut counts: HashMap<&str, i32> = HashMap::new();
-        for elem in self.required_fields.iter() {
-            counts.insert(elem, 0);
-        }
+        let mut counts: HashMap<String, i32> = HashMap::new();
+        // for elem in self.required_fields.iter() {
+        //     counts.insert(elem.to_owned().to_owned(), 0);
+        // }
 
         while let Some(Ok(line)) = self.lines.next() {
             if line.len() > 0 {
-                for elem in line
-                    .split(" ")
-                    .map(|entry| entry.split(":").next().expect("could not split by ':'"))
-                {
-                    *counts.entry(elem).or_insert(0) += 1;
+                for elem in line.split(" ").map(|entry| {
+                    [
+                        entry.split(":").next().expect("could not split by ':'"),
+                        entry.split(":").next().expect("could not split by ':'"),
+                    ]
+                }) {
+                    *counts.entry(elem[0].to_owned()).or_insert(0) += 1;
                 }
+            } else {
+                break;
             }
         }
-        Some("")
+        if counts.iter().count() == 0 {
+            return None;
+        }
+
+        let keys: HashSet<String> = HashSet::from_iter(counts.keys().map(|x| x.to_owned()));
+        let expected_keys: HashSet<String> = self
+            .required_fields
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<HashSet<_>>();
+        let diff_size = expected_keys.difference(&keys).count();
+        return Some(diff_size == 0);
     }
 }
 
@@ -61,5 +78,6 @@ fn main() {
     };
     let passport_generator =
         PassportGenerator::new(path, vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]);
-    println!("Hello, world!");
+    let num_pass = passport_generator.filter(|x| *x).count();
+    println!("Num passing {}", num_pass);
 }
