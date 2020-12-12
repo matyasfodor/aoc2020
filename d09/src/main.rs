2bbc15f28e304d4e6b597a7ab03934d4f60feeef
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-12 23:04:18 +0100

    feat: d09 second part

--- d09/src/main.rs
+++ d09/src/main.rs
@@ -7,11 +7,9 @@ use std::io::{self, BufRead};
 fn main() {
     let matches = App::new("AOC solution 9")
         .arg(Arg::with_name("test").short("t").long("test"))
-        .arg(Arg::with_name("second").short("s").long("second"))
         .get_matches();
 
-    // TODO store in argument
-    let preamble_length = 25;
+    let preamble_length = if matches.is_present("test") { 5 } else { 25 };
 
     let path = if matches.is_present("test") {
         "test.txt"
@@ -29,6 +27,7 @@ fn main() {
 
     let mut originals: VecDeque<usize> = preamble.iter().map(|x| x.to_owned()).collect();
 
+    let mut found_elem: Option<usize> = None;
     for elem in generator {
         let found = originals
             .iter()
@@ -36,9 +35,39 @@ fn main() {
             .map(|x| x[0] + x[1])
             .find(|x| elem == *x);
         if let None = found {
-            println!("Found {}", elem);
+            found_elem = Some(elem);
+            break;
         }
         originals.pop_front();
         originals.push_back(elem);
+    }
+    let first_result = found_elem.unwrap();
+    println!("First {}", first_result);
+
+    {
+        let file = File::open(path).expect("File not found");
+        let reader = io::BufReader::new(file).lines();
+        let generator = reader.map(|x| x.unwrap().parse::<usize>().unwrap());
+
+        let mut numbers: VecDeque<usize> = VecDeque::new();
+        let mut sum = 0;
+        let mut result: Option<usize> = None;
+
+        for elem in generator {
+            while sum + elem > first_result {
+                let removed_elem = numbers.pop_front().expect("Queue shouldn't be empty");
+                sum -= removed_elem;
+            }
+            if sum + elem == first_result {
+                numbers.push_back(elem);
+                let min = numbers.iter().min().unwrap();
+                let max = numbers.iter().max().unwrap();
+                result = Some(min + max);
+                break;
+            }
+            numbers.push_back(elem);
+            sum += elem;
+        }
+        println!("Second: {}", result.unwrap());
     }
 }
