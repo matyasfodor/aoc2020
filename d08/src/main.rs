9925b17c4bf55983e9998e9151f6e17bc352bd13
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-12 09:06:32 +0100

    feat: changing interface

--- d08/src/main.rs
+++ d08/src/main.rs
@@ -10,6 +10,13 @@ enum Instruction {
     Jmp(isize),
 }
 
+#[derive(Debug)]
+enum EvaluationResult {
+    LoopDetected { acc: isize, step: isize },
+    Finished { acc: isize, step: isize },
+    Error,
+}
+
 fn parse_line(line: &str) -> Instruction {
     match &line[..3] {
         "nop" => Instruction::Nop,
@@ -17,6 +24,109 @@ fn parse_line(line: &str) -> Instruction {
         "jmp" => Instruction::Jmp(line[4..].parse().expect("Could not parse jmp")),
         _ => panic!("Could not parse line"),
     }
+}
+
+fn first(instructions: &Vec<Instruction>) -> EvaluationResult {
+    let mut index = 0;
+    let mut visited_inidices: HashSet<usize> = HashSet::new();
+    let mut count = 0;
+    let result = loop {
+        if index > instructions.len() {
+            break EvaluationResult::Error;
+        } else if index == instructions.len() {
+            break EvaluationResult::Finished {
+                acc: count,
+                step: index as isize,
+            };
+        }
+        let instruction = instructions.get(index).expect("Index out of bounds");
+        match instruction {
+            Instruction::Nop => {
+                let next_index = index + 1;
+                if visited_inidices.contains(&next_index) {
+                    break EvaluationResult::LoopDetected {
+                        acc: count,
+                        step: index as isize,
+                    };
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+            }
+            Instruction::Acc(acc_count) => {
+                let next_index = index + 1;
+                if visited_inidices.contains(&next_index) {
+                    break EvaluationResult::LoopDetected {
+                        acc: count,
+                        step: index as isize,
+                    };
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+                count += acc_count;
+            }
+            Instruction::Jmp(jmp_count) => {
+                let next_index = ((index as isize) + jmp_count) as usize;
+                if visited_inidices.contains(&next_index) {
+                    break EvaluationResult::LoopDetected {
+                        acc: count,
+                        step: index as isize,
+                    };
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+            }
+        }
+    };
+    result
+}
+
+fn second(instructions: &Vec<Instruction>) {
+    let mut index = 0;
+    let mut visited_inidices: HashSet<usize> = HashSet::new();
+    let mut count = 0;
+    let result = loop {
+        if index > instructions.len() {
+            break None;
+        }
+        if index == instructions.len() {
+            break Some(count);
+        }
+        let instruction = instructions.get(index).expect("Index out of bounds");
+        match instruction {
+            Instruction::Nop => {
+                let next_index = index + 1;
+                if visited_inidices.contains(&next_index) {
+                    break None;
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+            }
+            Instruction::Acc(acc_count) => {
+                let next_index = index + 1;
+                if visited_inidices.contains(&next_index) {
+                    break None;
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+                count += acc_count;
+            }
+            Instruction::Jmp(jmp_count) => {
+                let next_index = ((index as isize) + jmp_count) as usize;
+                if visited_inidices.contains(&next_index) {
+                    break None;
+                } else {
+                    visited_inidices.insert(next_index);
+                }
+                index = next_index;
+            }
+        }
+    };
+    println!("Final count {}", result.unwrap());
 }
 
 fn main() {
@@ -35,46 +145,16 @@ fn main() {
         .lines()
         .map(|x| parse_line(x.unwrap().as_str()))
         .collect();
-    let mut index = 0;
-    let mut visited_inidices: HashSet<usize> = HashSet::new();
-    let mut count = 0;
-    loop {
-        let instruction = instructions.get(index).expect("Index out of bounds");
-        match instruction {
-            Instruction::Nop => {
-                let next_index = index + 1;
-                if visited_inidices.contains(&next_index) {
-                    break;
-                } else {
-                    visited_inidices.insert(next_index);
-                }
-                index = next_index;
-            }
-            Instruction::Acc(acc_count) => {
-                let next_index = index + 1;
-                if visited_inidices.contains(&next_index) {
-                    break;
-                } else {
-                    visited_inidices.insert(next_index);
-                }
-                index = next_index;
-                count += acc_count;
-            }
-            Instruction::Jmp(jmp_count) => {
-                let next_index = ((index as isize) + jmp_count) as usize;
-                if visited_inidices.contains(&next_index) {
-                    break;
-                } else {
-                    visited_inidices.insert(next_index);
-                }
-                index = next_index;
-            }
-        }
 
-        //match let elem = instructions.get(index) {
-        //    Ok(instruction) => println!("found"),
-        //    None => panic!("instruction could not be found"),
-        //}
+    if matches.is_present("second") {
+        second(&instructions);
+    } else {
+        match first(&instructions) {
+            EvaluationResult::LoopDetected {
+                acc: result,
+                step: _,
+            } => println!("Loop detected, acc: {}", result),
+            smth => println!("Did not converge {:?}", smth),
+        }
     }
-    println!("Final count {}", count);
 }
