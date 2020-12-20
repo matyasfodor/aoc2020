ffe5d1f4e94fa65c8415651f1133e603548114da
Mátyás Fodor <matyas.fodor@gmail.com>
2020-12-20 20:17:48 +0100

    feat: convolution

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -1,7 +1,31 @@
 use clap::{App, Arg};
-use ndarray::{array, concatenate, Array, Array2, Axis};
+use ndarray::{Array, Array2, Axis};
 use std::fs::File;
 use std::io::{self, BufRead};
+
+fn neighbor_counter(arr: &Array2<usize>) -> Array2<usize> {
+    let shape = arr.shape();
+    let mut ret = Array::zeros((shape[0], shape[1]));
+
+    for x in 0..(shape[0] as isize) {
+        for y in 0..(shape[1] as isize) {
+            let mut counter = 0;
+            for i in (x as isize - 1)..(x as isize + 2) {
+                if i < 0 || i >= (shape[0] as isize) {
+                    continue;
+                }
+                for j in (y - 1)..(y + 2) {
+                    if (x as isize == i && y == j) || j < 0 || j >= (shape[1] as isize) {
+                        continue;
+                    }
+                    counter += arr.get((i as usize, j as usize)).unwrap()
+                }
+            }
+            *ret.get_mut((x as usize, y as usize)).unwrap() = counter;
+        }
+    }
+    ret
+}
 
 fn main() {
     let matches = App::new("AOC solution 8")
@@ -38,4 +62,19 @@ fn main() {
         row.assign(&new_line);
     }
     println!("Hello, world! {:?}", floor);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_fun() {
+        assert_eq!(
+            super::neighbor_counter(&ndarray::arr2(&[[1, 2], [3, 4]])),
+            ndarray::arr2(&[[9, 8], [7, 6]])
+        );
+        assert_eq!(
+            super::neighbor_counter(&ndarray::arr2(&[[0, 1, 0], [1, 1, 1], [0, 1, 0]])),
+            ndarray::arr2(&[[3, 3, 3], [3, 4, 3], [3, 3, 3]])
+        );
+    }
 }
